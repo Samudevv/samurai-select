8e607f9a6c8a84703a36d722ebe892f16c2857db
PucklaJ <jonaas.pucher000000@gmail.com>
2023-10-10 18:31:27 +0200

    Add text next to the selection box

--- app.go
+++ app.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"fmt"
 
 	samure "github.com/PucklaJ/samurai-render-go"
 	samure_cairo "github.com/PucklaJ/samurai-render-go/backends/cairo"
@@ -23,7 +24,8 @@ type App struct {
 	backgroundColor [4]float64
 	selectionColor  [4]float64
 	borderColor     [4]float64
-	borderWidth     float64
+	textColor       [4]float64
+	padding         float64
 }
 
 func (a App) GetSelection() (samure.Rect, error) {
@@ -122,11 +124,14 @@ func (a *App) OnRender(ctx samure.Context, layerSurface samure.LayerSurface, o s
 		start[1], end[1] = end[1], start[1]
 	}
 
-	if o.RectInOutput(int(start[0]), int(start[1]), int(end[0]-start[0]), int(end[1]-start[1])) {
-		start[0] = o.RelX(start[0])
-		start[1] = o.RelY(start[1])
-		end[0] = o.RelX(end[0])
-		end[1] = o.RelY(end[1])
+	xGlobal := start[0]
+	yGlobal := start[1]
+	w := end[0] - start[0]
+	h := end[1] - start[1]
+
+	if o.RectInOutput(int(xGlobal), int(yGlobal), int(w), int(h)) {
+		x := o.RelX(xGlobal)
+		y := o.RelY(yGlobal)
 
 		// Render the selection
 		c.SetSourceRGBA(
@@ -135,7 +140,7 @@ func (a *App) OnRender(ctx samure.Context, layerSurface samure.LayerSurface, o s
 			a.selectionColor[2],
 			a.selectionColor[3],
 		)
-		c.Rectangle(start[0], start[1], end[0]-start[0], end[1]-start[1])
+		c.Rectangle(x, y, w, h)
 		c.Fill()
 		// Render the border of the selection
 		c.SetSourceRGBA(
@@ -144,9 +149,67 @@ func (a *App) OnRender(ctx samure.Context, layerSurface samure.LayerSurface, o s
 			a.borderColor[2],
 			a.borderColor[3],
 		)
-		c.Rectangle(start[0], start[1], end[0]-start[0], end[1]-start[1])
-		c.SetLineWidth(a.borderWidth)
+		c.Rectangle(x, y, w, h)
+		c.SetLineWidth(flags.BorderWidth)
 		c.Stroke()
+	}
+
+	if flags.Text {
+		x := o.RelX(xGlobal)
+		y := o.RelY(yGlobal)
+
+		widthStr := fmt.Sprintf("%.0f", w)
+		heightStr := fmt.Sprintf("%.0f", h)
+		xStr := fmt.Sprintf("X: %.0f", x)
+		yStr := fmt.Sprintf("Y: %.0f", y)
+
+		c.SelectFontFace(flags.Font, cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
+		c.SetFontSize(flags.FontSize)
+		c.SetSourceRGBA(
+			a.textColor[0],
+			a.textColor[1],
+			a.textColor[2],
+			a.textColor[3],
+		)
+		widthExt := c.TextExtents(widthStr)
+		heightExt := c.TextExtents(heightStr)
+		xExt := c.TextExtents(xStr)
+		yExt := c.TextExtents(yStr)
+
+		widthTextPos := [2]float64{
+			x + w/2.0 - widthExt.Width/2.0,
+			y + h + widthExt.Height + a.padding,
+		}
+		heightTextPos := [2]float64{
+			x + w + a.padding,
+			y + h/2.0 + heightExt.Height/2.0,
+		}
+		xTextPos := [2]float64{
+			x,
+			y - a.padding,
+		}
+		yTextPos := [2]float64{
+			x - yExt.Width - a.padding,
+			y + yExt.Height,
+		}
+
+		// Only render text if it's inside the output
+		if o.RectInOutput(int(widthTextPos[0])+o.X, int(widthTextPos[1])+o.Y, int(widthExt.Width), int(widthExt.Height)) {
+			c.MoveTo(widthTextPos[0], widthTextPos[1])
+			c.ShowText(widthStr)
+		}
+		if o.RectInOutput(int(heightTextPos[0])+o.X, int(heightTextPos[1])+o.Y, int(heightExt.Width), int(heightExt.Height)) {
+			c.MoveTo(heightTextPos[0], heightTextPos[1])
+			c.ShowText(heightStr)
+		}
+		if o.RectInOutput(int(xTextPos[0])+o.X, int(xTextPos[1])+o.Y, int(xExt.Width), int(xExt.Height)) {
+			c.MoveTo(xTextPos[0], xTextPos[1])
+			c.ShowText(xStr)
+		}
+		if o.RectInOutput(int(yTextPos[0])+o.X, int(yTextPos[1])+o.Y, int(yExt.Width), int(yExt.Height)) {
+			c.MoveTo(yTextPos[0], yTextPos[1])
+			c.ShowText(yStr)
+		}
 	}
 }
 
--- flags.go
+++ flags.go
@@ -12,8 +12,13 @@ var flags struct {
 	BackgroundColor string `long:"background-color" description:"Set the clear color that fills the screen" default:"#FFFFFF40"`
 	SelectionColor  string `long:"selection-color" description:"Set the color that is used to draw the inside of the selection box" default:"#00000000"`
 	BorderColor     string `long:"border-color" description:"Set the color that is used to draw the border around the selection box" default:"#000000FF"`
+	TextColor       string `long:"text-color" description:"Set the color that is used for the text" default:"#000000FF"`
 
 	BorderWidth float64 `long:"border-width" description:"The width of the border in pixels" default:"2.0"`
+	Text        bool    `short:"t" long:"text" description:"Display the selection position and dimensions next to the selection box"`
+	Font        string  `long:"font" description:"Set the font family of the text" default:"sans-serif"`
+	FontSize    float64 `long:"font-size" description:"Set the font size of the text" default:"16"`
+	TextPadding float64 `long:"text-padding" description:"The distance between the selection box and each text" default:"10"`
 }
 
 func CreateApp(argv []string) (*App, error) {
@@ -33,12 +38,12 @@ func CreateApp(argv []string) (*App, error) {
 	a.backgroundColor = parseColor(flags.BackgroundColor)
 	a.selectionColor = parseColor(flags.SelectionColor)
 	a.borderColor = parseColor(flags.BorderColor)
+	a.textColor = parseColor(flags.TextColor)
 	if flags.BorderWidth < 0.0 {
 		fmt.Fprintf(os.Stderr, "--border-width values below zero are invalid\n")
-		a.borderWidth = 0.0
-	} else {
-		a.borderWidth = flags.BorderWidth
+		flags.BorderWidth = 0.0
 	}
+	a.padding = flags.TextPadding + flags.BorderWidth/2.0
 	return a, nil
 }
 
