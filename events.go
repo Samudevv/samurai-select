2478f8aa60ac4de1e28066e4dd7308110175dfa3
PucklaJ <jonaas.pucher000000@gmail.com>
2023-10-14 13:39:48 +0200

    Create pointerDown, pointerUp and pointerMove

--- events.go
+++ events.go
@@ -6,106 +6,195 @@ import (
 	samure "github.com/PucklaJ/samurai-render-go"
 )
 
-func (a *App) OnEvent(ctx samure.Context, event interface{}) {
-	switch e := event.(type) {
-	case samure.EventPointerButton:
-		switch a.state {
-		case StateNone:
-			if e.Button == samure.ButtonLeft && e.State == samure.StatePressed {
-				a.anchor = a.pointer
-				a.computeStartEnd()
+func (a *App) pointerDown(ctx samure.Context, px, py float64) {
+	switch a.state {
+	case StateNone:
+		a.anchor[0], a.anchor[1] = px, py
+		a.computeStartEnd(px, py, px, py)
 
+		a.state = StateDragNormal
+		ctx.SetRenderState(samure.RenderStateOnce)
+	case StateAlter:
+		x := a.start[0]
+		y := a.start[1]
+		w := a.end[0] - a.start[0]
+		h := a.end[1] - a.start[1]
+
+		if a.pointerInGrabber(px, py, x, y) {
+			a.offset[0] = x - px
+			a.offset[1] = y - py
+			a.state = StateDragTopLeft
+		} else if a.pointerInGrabber(px, py, x+w/2.0, y) {
+			a.offset[0] = x + w/2.0 - px
+			a.offset[1] = y - py
+			a.state = StateDragTop
+		} else if a.pointerInGrabber(px, py, x+w, y) {
+			a.offset[0] = x + w - px
+			a.offset[1] = y - py
+			a.state = StateDragTopRight
+		} else if a.pointerInGrabber(px, py, x+w, y+h/2.0) {
+			a.offset[0] = x + w - px
+			a.offset[1] = y + h/2.0 - py
+			a.state = StateDragRight
+		} else if a.pointerInGrabber(px, py, x+w, y+h) {
+			a.offset[0] = x + w - px
+			a.offset[1] = y + h - py
+			a.state = StateDragBottomRight
+		} else if a.pointerInGrabber(px, py, x+w/2.0, y+h) {
+			a.offset[0] = x + w/2.0 - px
+			a.offset[1] = y + h - py
+			a.state = StateDragBottom
+		} else if a.pointerInGrabber(px, py, x, y+h) {
+			a.offset[0] = x - px
+			a.offset[1] = y + h - py
+			a.state = StateDragBottomLeft
+		} else if a.pointerInGrabber(px, py, x, y+h/2.0) {
+			a.offset[0] = x - px
+			a.offset[1] = y + h/2.0 - py
+			a.state = StateDragLeft
+		} else {
+			sel := samure.Rect{
+				X: int(x),
+				Y: int(y),
+				W: int(w),
+				H: int(h),
+			}
+
+			if sel.PointInOutput(int(px), int(py)) {
+				a.state = StateDragMiddle
+			} else {
+				a.grabberAnim = 0.0
+				a.anchor[0], a.anchor[1] = px, py
+				a.computeStartEnd(px, py, px, py)
 				a.state = StateDragNormal
 				ctx.SetRenderState(samure.RenderStateOnce)
 			}
-		case StateDragNormal:
-			if e.Button == samure.ButtonLeft && e.State == samure.StateReleased {
-				if flags.AlterSelection {
-					a.state = StateAlter
-					ctx.SetRenderState(samure.RenderStateOnce)
-				} else {
-					ctx.SetRunning(false)
-				}
-			}
-		case StateAlter:
-			if e.Button == samure.ButtonLeft && e.State == samure.StatePressed {
-				px := a.pointer[0]
-				py := a.pointer[1]
-				x := a.start[0]
-				y := a.start[1]
-				w := a.end[0] - a.start[0]
-				h := a.end[1] - a.start[1]
+		}
+	}
+}
 
-				if a.pointerInGrabber(px, py, x, y) {
-					a.offset[0] = x - px
-					a.offset[1] = y - py
-					a.state = StateDragTopLeft
-				} else if a.pointerInGrabber(px, py, x+w/2.0, y) {
-					a.offset[0] = x + w/2.0 - px
-					a.offset[1] = y - py
-					a.state = StateDragTop
-				} else if a.pointerInGrabber(px, py, x+w, y) {
-					a.offset[0] = x + w - px
-					a.offset[1] = y - py
-					a.state = StateDragTopRight
-				} else if a.pointerInGrabber(px, py, x+w, y+h/2.0) {
-					a.offset[0] = x + w - px
-					a.offset[1] = y + h/2.0 - py
-					a.state = StateDragRight
-				} else if a.pointerInGrabber(px, py, x+w, y+h) {
-					a.offset[0] = x + w - px
-					a.offset[1] = y + h - py
-					a.state = StateDragBottomRight
-				} else if a.pointerInGrabber(px, py, x+w/2.0, y+h) {
-					a.offset[0] = x + w/2.0 - px
-					a.offset[1] = y + h - py
-					a.state = StateDragBottom
-				} else if a.pointerInGrabber(px, py, x, y+h) {
-					a.offset[0] = x - px
-					a.offset[1] = y + h - py
-					a.state = StateDragBottomLeft
-				} else if a.pointerInGrabber(px, py, x, y+h/2.0) {
-					a.offset[0] = x - px
-					a.offset[1] = y + h/2.0 - py
-					a.state = StateDragLeft
-				} else {
-					sel := samure.Rect{
-						X: int(x),
-						Y: int(y),
-						W: int(w),
-						H: int(h),
-					}
+func (a *App) pointerUp(ctx samure.Context) {
+	switch a.state {
+	case StateDragNormal:
+		if flags.AlterSelection {
+			a.state = StateAlter
+			ctx.SetRenderState(samure.RenderStateOnce)
+		} else {
+			ctx.SetRunning(false)
+		}
+	case StateDragTopLeft:
+		fallthrough
+	case StateDragTop:
+		fallthrough
+	case StateDragTopRight:
+		fallthrough
+	case StateDragRight:
+		fallthrough
+	case StateDragBottomRight:
+		fallthrough
+	case StateDragBottom:
+		fallthrough
+	case StateDragBottomLeft:
+		fallthrough
+	case StateDragLeft:
+		fallthrough
+	case StateDragMiddle:
+		a.state = StateAlter
+	}
+}
 
-					if sel.PointInOutput(int(a.pointer[0]), int(a.pointer[1])) {
-						a.state = StateDragMiddle
-					} else {
-						a.grabberAnim = 0.0
-						a.anchor = a.pointer
-						a.computeStartEnd()
-						a.state = StateDragNormal
-						ctx.SetRenderState(samure.RenderStateOnce)
-					}
-				}
-			}
-		case StateDragTopLeft:
-			fallthrough
-		case StateDragTop:
-			fallthrough
-		case StateDragTopRight:
-			fallthrough
-		case StateDragRight:
-			fallthrough
-		case StateDragBottomRight:
-			fallthrough
-		case StateDragBottom:
-			fallthrough
-		case StateDragBottomLeft:
-			fallthrough
-		case StateDragLeft:
-			fallthrough
-		case StateDragMiddle:
-			if e.Button == samure.ButtonLeft && e.State == samure.StateReleased {
-				a.state = StateAlter
+func (a *App) pointerMove(ctx samure.Context, px, py, dx, dy float64) {
+	pox := px + a.offset[0]
+	poy := py + a.offset[1]
+	x := a.start[0]
+	y := a.start[1]
+	w := a.end[0] - a.start[0]
+	h := a.end[1] - a.start[1]
+
+	switch a.state {
+	case StateDragNormal:
+		a.computeStartEnd(px, py, a.anchor[0], a.anchor[1])
+		ctx.SetRenderState(samure.RenderStateOnce)
+	case StateDragTopLeft:
+		w += x - pox
+		h += y - poy
+		x = pox
+		y = poy
+		a.start[0] = x
+		a.start[1] = y
+		a.end[0] = x + w
+		a.end[1] = y + h
+		a.handleOverlapAndAspectRatio()
+		ctx.SetRenderState(samure.RenderStateOnce)
+	case StateDragTop:
+		h += y - poy
+		y = poy
+		a.start[1] = y
+		a.end[1] = y + h
+		a.handleOverlapAndAspectRatio()
+		ctx.SetRenderState(samure.RenderStateOnce)
+	case StateDragTopRight:
+		w = pox - x
+		h += y - poy
+		y = poy
+		a.start[1] = y
+		a.end[0] = x + w
+		a.end[1] = y + h
+		a.handleOverlapAndAspectRatio()
+		ctx.SetRenderState(samure.RenderStateOnce)
+	case StateDragRight:
+		w = pox - x
+		a.end[0] = x + w
+		a.handleOverlapAndAspectRatio()
+		ctx.SetRenderState(samure.RenderStateOnce)
+	case StateDragBottomRight:
+		w = pox - x
+		h = poy - y
+		a.end[0] = x + w
+		a.end[1] = y + h
+		a.handleOverlapAndAspectRatio()
+		ctx.SetRenderState(samure.RenderStateOnce)
+	case StateDragBottom:
+		h = poy - y
+		a.end[1] = y + h
+		a.handleOverlapAndAspectRatio()
+		ctx.SetRenderState(samure.RenderStateOnce)
+	case StateDragBottomLeft:
+		w += x - pox
+		x = pox
+		h = poy - y
+		a.start[0] = x
+		a.end[0] = x + w
+		a.end[1] = y + h
+		a.handleOverlapAndAspectRatio()
+		ctx.SetRenderState(samure.RenderStateOnce)
+	case StateDragLeft:
+		w += x - pox
+		x = pox
+		a.start[0] = x
+		a.end[0] = x + w
+		a.handleOverlapAndAspectRatio()
+		ctx.SetRenderState(samure.RenderStateOnce)
+	case StateDragMiddle:
+		x += dx
+		y += dy
+		a.start[0] = x
+		a.start[1] = y
+		a.end[0] = x + w
+		a.end[1] = y + h
+		ctx.SetRenderState(samure.RenderStateOnce)
+	}
+}
+
+func (a *App) OnEvent(ctx samure.Context, event interface{}) {
+	switch e := event.(type) {
+	case samure.EventPointerButton:
+		if e.Button == samure.ButtonLeft {
+			switch e.State {
+			case samure.StatePressed:
+				a.pointerDown(ctx, a.pointer[0], a.pointer[1])
+			case samure.StateReleased:
+				a.pointerUp(ctx)
 			}
 		}
 	case samure.EventTouchDown:
@@ -117,125 +206,34 @@ func (a *App) OnEvent(ctx samure.Context, event interface{}) {
 		*a.touchID = e.TouchID
 		a.touchFocus = e.Output
 
-		a.anchor[0] = e.X + float64(e.Output.Geo().X)
-		a.anchor[1] = e.Y + float64(e.Output.Geo().Y)
-
-		a.pointer = a.anchor
-		a.start = a.anchor
-		a.end = a.start
-
-		a.state = StateDragNormal
-		ctx.SetRenderState(samure.RenderStateOnce)
+		a.pointer[0] = e.X + float64(e.Output.Geo().X)
+		a.pointer[1] = e.Y + float64(e.Output.Geo().Y)
+		a.pointerDown(ctx, a.pointer[0], a.pointer[1])
 	case samure.EventTouchUp:
 		if a.touchID == nil || *a.touchID != e.TouchID {
 			break
 		}
+		a.touchID = nil
 
-		switch a.state {
-		case StateDragNormal:
-			a.touchID = nil
-			ctx.SetRunning(false)
-		}
+		a.pointerUp(ctx)
 	case samure.EventPointerMotion:
 		px := e.X + float64(e.Seat.PointerFocus().Output().Geo().X)
 		py := e.Y + float64(e.Seat.PointerFocus().Output().Geo().Y)
 		dx := px - a.pointer[0]
 		dy := py - a.pointer[1]
-		a.pointer[0] = px
-		a.pointer[1] = py
-		px += a.offset[0]
-		py += a.offset[1]
-		x := a.start[0]
-		y := a.start[1]
-		w := a.end[0] - a.start[0]
-		h := a.end[1] - a.start[1]
-
-		switch a.state {
-		case StateDragNormal:
-			a.computeStartEnd()
-			ctx.SetRenderState(samure.RenderStateOnce)
-		case StateDragTopLeft:
-			w += x - px
-			h += y - py
-			x = px
-			y = py
-			a.start[0] = x
-			a.start[1] = y
-			a.end[0] = x + w
-			a.end[1] = y + h
-			a.handleOverlapAndAspectRatio()
-			ctx.SetRenderState(samure.RenderStateOnce)
-		case StateDragTop:
-			h += y - py
-			y = py
-			a.start[1] = y
-			a.end[1] = y + h
-			a.handleOverlapAndAspectRatio()
-			ctx.SetRenderState(samure.RenderStateOnce)
-		case StateDragTopRight:
-			w = px - x
-			h += y - py
-			y = py
-			a.start[1] = y
-			a.end[0] = x + w
-			a.end[1] = y + h
-			a.handleOverlapAndAspectRatio()
-			ctx.SetRenderState(samure.RenderStateOnce)
-		case StateDragRight:
-			w = px - x
-			a.end[0] = x + w
-			a.handleOverlapAndAspectRatio()
-			ctx.SetRenderState(samure.RenderStateOnce)
-		case StateDragBottomRight:
-			w = px - x
-			h = py - y
-			a.end[0] = x + w
-			a.end[1] = y + h
-			a.handleOverlapAndAspectRatio()
-			ctx.SetRenderState(samure.RenderStateOnce)
-		case StateDragBottom:
-			h = py - y
-			a.end[1] = y + h
-			a.handleOverlapAndAspectRatio()
-			ctx.SetRenderState(samure.RenderStateOnce)
-		case StateDragBottomLeft:
-			w += x - px
-			x = px
-			h = py - y
-			a.start[0] = x
-			a.end[0] = x + w
-			a.end[1] = y + h
-			a.handleOverlapAndAspectRatio()
-			ctx.SetRenderState(samure.RenderStateOnce)
-		case StateDragLeft:
-			w += x - px
-			x = px
-			a.start[0] = x
-			a.end[0] = x + w
-			a.handleOverlapAndAspectRatio()
-			ctx.SetRenderState(samure.RenderStateOnce)
-		case StateDragMiddle:
-			x += dx
-			y += dy
-			a.start[0] = x
-			a.start[1] = y
-			a.end[0] = x + w
-			a.end[1] = y + h
-			ctx.SetRenderState(samure.RenderStateOnce)
-		}
+		a.pointer[0], a.pointer[1] = px, py
+		a.pointerMove(ctx, px, py, dx, dy)
 	case samure.EventTouchMotion:
 		if a.touchID == nil || *a.touchID != e.TouchID {
 			break
 		}
 
-		a.pointer[0] = e.X + float64(a.touchFocus.Geo().X)
-		a.pointer[1] = e.Y + float64(a.touchFocus.Geo().Y)
-
-		switch a.state {
-		case StateDragNormal:
-			a.computeStartEnd()
-			ctx.SetRenderState(samure.RenderStateOnce)
-		}
+		px := e.X + float64(a.touchFocus.Geo().X)
+		py := e.Y + float64(a.touchFocus.Geo().Y)
+		dx := px - a.pointer[0]
+		dy := py - a.pointer[1]
+		a.pointer[0], a.pointer[1] = px, py
+		a.pointerMove(ctx, px, py, dx, dy)
 	case samure.EventPointerEnter:
 		e.Seat.SetPointerShape(samure.CursorShapeCrosshair)
 	case samure.EventKeyboardKey:
@@ -256,28 +254,28 @@ func (a *App) OnEvent(ctx samure.Context, event interface{}) {
 	}
 }
 
-func (a *App) computeStartEnd() {
-	width := math.Abs(a.pointer[0] - a.anchor[0])
-	height := math.Abs(a.pointer[1] - a.anchor[1])
+func (a *App) computeStartEnd(px, py, ax, ay float64) {
+	width := math.Abs(px - ax)
+	height := math.Abs(py - ay)
 
 	if a.aspect != 0.0 {
 		width = math.Max(width, height*a.aspect)
 		height = math.Max(height, width/a.aspect)
 	}
 
-	if a.pointer[0] < a.anchor[0] {
-		a.start[0] = a.anchor[0] - width - 1
-		a.end[0] = a.anchor[0]
+	if px < ax {
+		a.start[0] = ax - width - 1
+		a.end[0] = ax
 	} else {
-		a.start[0] = a.anchor[0]
-		a.end[0] = a.anchor[0] + width + 1
+		a.start[0] = ax
+		a.end[0] = ax + width + 1
 	}
-	if a.pointer[1] < a.anchor[1] {
-		a.start[1] = a.anchor[1] - height - 1
-		a.end[1] = a.anchor[1]
+	if py < ay {
+		a.start[1] = ay - height - 1
+		a.end[1] = ay
 	} else {
-		a.start[1] = a.anchor[1]
-		a.end[1] = a.anchor[1] + height + 1
+		a.start[1] = ay
+		a.end[1] = ay + height + 1
 	}
 }
 
